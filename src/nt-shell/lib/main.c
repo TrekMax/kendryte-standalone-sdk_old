fd0bb8c84d17c0429ca064cc726757279c425a00
QinYUN575 <QinYUN575@Gmail.com>
2020-06-09 00:43:37 +0800

    增加 nt-hell 移植

---
+++ src/nt-shell/lib/main.c
@@ -0,0 +1,117 @@
+/* Copyright 2018 Canaan Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <bsp.h>
+#include <sysctl.h>
+#include "board_config.h"
+#include "syslog.h"
+#include "uart.h"
+
+static const char *TAG = "main";
+
+// #include "chip.h"
+#include "ntlibc.h"
+#include "ntshell.h"
+// #include "uart.h"
+
+#define UNUSED_VARIABLE(N) \
+    do                     \
+    {                      \
+        (void)(N);         \
+    } while(0)
+
+static int func_read(char *buf, int cnt, void *extobj)
+{
+    int i;
+    UNUSED_VARIABLE(extobj);
+    for(i = 0; i < cnt; i++)
+    {
+        buf[i] = uart_getc();
+    }
+    return cnt;
+}
+
+static int func_write(const char *buf, int cnt, void *extobj)
+{
+    int i;
+    UNUSED_VARIABLE(extobj);
+    for(i = 0; i < cnt; i++)
+    {
+        uart_putc(buf[i]);
+    }
+    return cnt;
+}
+
+static int func_callback(const char *text, void *extobj)
+{
+    ntshell_t *ntshell = (ntshell_t *)extobj;
+    UNUSED_VARIABLE(ntshell);
+    UNUSED_VARIABLE(extobj);
+    if(ntlibc_strlen(text) > 0)
+    {
+        printk("User input text:'");
+        printk(text);
+        printk("'\r\n");
+    }
+    return 0;
+}
+
+int core1_function(void *ctx)
+{
+    ntshell_t ntshell;
+    uint64_t core = current_coreid();
+    LOGI(TAG, "Core %ld Hello world", core);
+
+    ntshell_init(
+        &ntshell,
+        func_read,
+        func_write,
+        func_callback,
+        (void *)&ntshell);
+    ntshell_set_prompt(&ntshell, "BlueTank>");
+    ntshell_execute(&ntshell);
+    
+    while(1)
+        ;
+}
+
+int main(void)
+{
+    uint32_t freq = 0;
+    freq = sysctl_pll_set_freq(SYSCTL_PLL0, 800000000);
+    uint64_t core = current_coreid();
+    int data;
+    printk(LOG_COLOR_W "-------------------------------\r\n");
+    printk(LOG_COLOR_W "Sipeed@QinYUN575\r\n");
+    printk(LOG_COLOR_W "Compile@ %s %s\r\n", __DATE__, __TIME__);
+    printk(LOG_COLOR_W "Board: " LOG_COLOR_E BOARD_NAME "\r\n");
+    printk(LOG_COLOR_W "pll freq: %dhz\r\n", freq);
+    printk(LOG_COLOR_W "-------------------------------\r\n");
+
+    LOGI(TAG, "Core %ld Hello world", core);
+    register_core1(core1_function, NULL);
+
+    // /* Clear stdin buffer before scanf */
+    // sys_stdin_flush();
+
+    // scanf("%d", &data);
+    // LOGI(TAG, "\r\nData is %d", data);
+    // printk(LOG_COLOR_W "-------------END---------------\r\n");
+    // while(1)
+    // {
+    //     LOGI(TAG, "Hello K210!");
+    //     msleep(1000);
+    // }
+    return 0;
+}
